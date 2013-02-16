8777fd6c47596ac31541b363c46c9a022555c7cb
Matt <matt@primestudiosllc.com>
2013-02-15 21:01:01 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slipaid::Application.config.secret_token = '20655c1e5444852dd135468b0356e35f32e20174fcb3c216239be5fe420ed8ad843709c6e4083f599af6b3f5f02250c94133ac0477b87ff14f3f730c72478168'
